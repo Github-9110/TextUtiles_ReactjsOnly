9c39c872c09433bcdf0ab695a742c919838bc59b
Your Name <you@example.com>
2025-01-04 15:45:09 +0530

    Nav bg change

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,8 @@ import PropTypes from 'prop-types'
 
 export default function Navbar(props) {
   return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-secondary">
+    <nav className="navbar navbar-expand-lg navbar-light bg-info">
+    
   <a className="navbar-brand" href="/">{props.title}</a>
   <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span className="navbar-toggler-icon"></span>
@@ -18,8 +19,6 @@ export default function Navbar(props) {
       <li className="nav-item">
         <a className="nav-link" href="/">{props.aboutTitle}</a>
       </li>
-      
-     
     </ul>
     <form className="form-inline my-2 my-lg-0">
       <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" />
