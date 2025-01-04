57149e715c90c4f8dc857e329658538f715e732c
Your Name <you@example.com>
2025-01-04 19:43:05 +0530

    text case clear, remove exrap space

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,9 +4,9 @@ import PropTypes from 'prop-types'
 
 export default function Navbar(props) {
   return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-info">
+    <nav className="navbar navbar-expand-lg navbar-light bg-dark">
     
-  <a className="navbar-brand" href="/">{props.title}</a>
+  <a className="navbar-brand text-light" href="/">{props.title}</a>
   <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span className="navbar-toggler-icon"></span>
   </button>
@@ -14,15 +14,15 @@ export default function Navbar(props) {
   <div className="collapse navbar-collapse" id="navbarSupportedContent">
     <ul className="navbar-nav mr-auto">
       <li className="nav-item active">
-        <a className="nav-link" href="/">{props.homeTitle} <span className="sr-only">(current)</span></a>
+        <a className="nav-link text-white" href="/">{props.homeTitle} <span className="sr-only">(current)</span></a>
       </li>
       <li className="nav-item">
-        <a className="nav-link" href="/">{props.aboutTitle}</a>
+        <a className="nav-link text-white" href="/">{props.aboutTitle}</a>
       </li>
     </ul>
     <form className="form-inline my-2 my-lg-0">
       <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" />
-      <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+      <button className="btn btn-success my-2 my-sm-0" type="submit">Search</button>
     </form>
   </div>
 </nav>
--- src/components/Textform.jsx
+++ src/components/Textform.jsx
@@ -1,9 +1,15 @@
 import React, {useState} from 'react';
 
 
-export default function Textform(props) {
-   
 
+export default function Textform(props) {
+    const [text, setText] = useState("");
+
+   let myStyle={ 
+         height: 'auto',
+         minHeight: '100px', 
+         maxHeight: '500px',
+         }; 
     /* Event handing  */
     const handleUperCase = ()=>{
         // console.log("UpperCase");
@@ -21,16 +27,48 @@ export default function Textform(props) {
         let lowerText = text.toLowerCase();
         setText(lowerText);
     }
-    const [text, setText] = useState("Enter Text Here");
-  return (
-    <div className="container">
-        <div className=" mb-3">
-            <h1>{props.heading}</h1>
-            <textarea className="form-control" value={text}  onChange={handleOnchange} id="MyTextForm" rows="8"></textarea>
-        </div>
-        <button className="btn btn-primary m-2" onClick={handleUperCase}  >Convert to Uppercase</button>
-        <button className="btn btn-primary m-2" onClick={handleLowerCase} >Convert to Lowercase</button>
 
+    const handleCopyText = ()=>{
+        let textBox = document.getElementById("MyTextForm");
+        navigator.clipboard.writeText(textBox.value);
+    }
+    const handleExtraSpace = ()=>{
+        let extraspace = text.trim()  
+        .replace(/\s+/g, ' ')  
+        .replace(/[_-](.)/g, (_, char) => char.toUpperCase())  
+        .replace(/^([A-Z])/, match => match.toLowerCase());  
+        setText(extraspace);
+    }
+
+    const handleClearText = ()=>{
+        setText("");
+    }
+
+
+  return (
+    <>
+    <div className="container">
+    <div className=" mb-3">
+        <h1>{props.heading}</h1>
+        <textarea className="form-control" value={text}  onChange={handleOnchange} id="MyTextForm" rows="8"></textarea>
     </div>
+    <button className="btn btn-primary m-2" onClick={handleUperCase}  >Convert to Uppercase</button>
+    <button className="btn btn-primary m-2" onClick={handleLowerCase} >Convert to Lowercase</button>
+    <button className="btn btn-primary m-2" onClick={handleExtraSpace} >Extra Space</button>
+    <button className="btn btn-primary m-2" onClick={handleCopyText} >Copy Text</button>
+    <button className="btn btn-primary m-2" onClick={handleClearText} >Clean</button>
+
+
+    <div className="my-4">
+    <h5>Text summary</h5>
+    <p>{text.split(" ").length} words and {text.length} characters</p>
+    <p>{0.008*text.split(" ").length} per minute read word. </p>
+    <h5>Preview</h5>
+    <p>{text.length>0?text:" Enter your text in Text area to Preview."}</p>
+    <p> <canvas id="screenshotCanvas"></canvas></p>
+   </div>
+   </div>
+   
+</>
   )
 }
